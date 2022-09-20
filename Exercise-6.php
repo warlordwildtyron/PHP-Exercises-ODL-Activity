7c714b39712c71553beeb3f0bcb269b12dc2d81b
warlordwildtyron <81098671+warlordwildtyron@users.noreply.github.com>
2022-09-20 20:04:40 +0800

    Add files via upload

---
+++ Exercise-6.php
@@ -0,0 +1,26 @@
+<?php
+
+
+$total=0;
+
+for($i=1;$i<=12;$i++){
+	$total+=$i;
+	echo "<br>".$total;
+}
+
+for($i=1;$i<=12;$i++):
+	$total+=$i;
+	echo "<br>".$total;
+endfor;
+
+
+
+
+
+
+?>
+
+
+
+
+
---
+++ Exercise-7.php
@@ -0,0 +1,12 @@
+<?php
+echo "<table border =\"1\" style='border-collapse: collapse'>";
+	for ($row=1; $row <= 7; $row++) { 
+		echo "<tr> \n";
+		for ($col=1; $col <= 7; $col++) { 
+		   $p = $col * $row;
+		   echo "<td>$p</td> \n";
+		   	}
+	  	    echo "</tr>";
+		}
+		echo "</table>";
+?>
